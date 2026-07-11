1137c8bd51aa04378de5042e5f59f814f200393f
IvanPSG-BR <ivan.psg@protonmail.com>
2026-07-11 00:32:53 -0300

    fix: remove tratamento de erros genérico de `transactions.controller.ts`

--- api/src/modules/transactions/transactions.controller.ts
+++ api/src/modules/transactions/transactions.controller.ts
@@ -11,13 +11,8 @@ export default class TransactionsController {
   }
 
   public create = async (req: FastifyRequest<{ Body: TransactionCreateDTO }>, rep: FastifyReply): Promise<FastifyReply> => {
-    try {
-      const transactionCreated = await this.transactionsService.create(req.body)
-      return rep.status(201).send({ details: "Created", data: transactionCreated })
-    } catch (error: unknown) {
-      console.error(`Algo de errado não está certo: ${error}`)
-      return rep.status(500).send({ details: "Internal Server Error" })
-    }
+    const transactionCreated = await this.transactionsService.create(req.body)
+    return rep.status(201).send(transactionCreated)
   }
 
   public list = async (req: FastifyRequest<{
@@ -36,53 +31,33 @@ export default class TransactionsController {
       orderedByUpdatedAt?: ORDER
     }
   }>, rep: FastifyReply): Promise<FastifyReply> => {
-    try {
-      const transactionList = await this.transactionsService.list(req.query.limit, req.query.offset, {
-        season: req.query.season ?? "ALL",
-        valueRange: [Number(req.query.valueStart) ?? 1, Number(req.query.valueEnd) ?? 999999999],
-        dayRange: [Number(req.query.dayStart) ?? 1, Number(req.query.dayEnd) ?? 28],
-        deleted: Boolean(req.query.deleted) ?? false,
-        orderedBy: {
-          totalValue: req.query.orderedByTotalValue ?? ORDER.ASC,
-          day: req.query.orderedByDay ?? ORDER.ASC,
-          createdAt: req.query.orderedByCreatedAt ?? ORDER.ASC,
-          updatedAt: req.query.orderedByCreatedAt ?? ORDER.ASC
-        }
-      })
-      return rep.status(200).send({ details: "OK", data: transactionList })
-    } catch (error: unknown) {
-      console.error(`Algo de errado não está certo: ${error}`)
-      return rep.status(404).send({ details: "Not Found" })
-    }
+    const transactionList = await this.transactionsService.list(req.query.limit, req.query.offset, {
+      season: req.query.season ?? "ALL",
+      valueRange: [Number(req.query.valueStart) ?? 1, Number(req.query.valueEnd) ?? 999999999],
+      dayRange: [Number(req.query.dayStart) ?? 1, Number(req.query.dayEnd) ?? 28],
+      deleted: Boolean(req.query.deleted) ?? false,
+      orderedBy: {
+        totalValue: req.query.orderedByTotalValue ?? ORDER.ASC,
+        day: req.query.orderedByDay ?? ORDER.ASC,
+        createdAt: req.query.orderedByCreatedAt ?? ORDER.ASC,
+        updatedAt: req.query.orderedByCreatedAt ?? ORDER.ASC
+      }
+    })
+    return rep.status(200).send(transactionList)
   }
 
   public find = async (req: FastifyRequest<{ Params: { id: number } }>, rep: FastifyReply): Promise<FastifyReply> => {
-    try {
-      const transaction = await this.transactionsService.find(Number(req.params.id))
-      return rep.status(200).send({ details: "OK", data: transaction })
-    } catch (error: unknown) {
-      console.error(`Algo de errado não está certo: ${error}`)
-      return rep.status(404).send({ details: "Not Found" })
-    }
+    const transaction = await this.transactionsService.find(Number(req.params.id))
+    return rep.status(200).send(transaction)
   }
 
   public update = async (req: FastifyRequest<{ Params: { id: number }, Body: TransactionBaseDTO }>, rep: FastifyReply): Promise<FastifyReply> => {
-    try {
-      const transaction = await this.transactionsService.update(Number(req.params.id), req.body)
-      return rep.status(200).send({ details: "OK", data: transaction })
-    } catch (error: unknown) {
-      console.error(`Algo de errado não está certo: ${error}`)
-      return rep.status(500).send({ details: "Internal Server Error" })
-    }
+    const transaction = await this.transactionsService.update(Number(req.params.id), req.body)
+    return rep.status(200).send(transaction)
   }
 
   public delete = async (req: FastifyRequest<{ Params: { id: number } }>, rep: FastifyReply): Promise<FastifyReply> => {
-    try {
-      const transaction = await this.transactionsService.delete(Number(req.params.id))
-      return rep.status(200).send({ details: "OK", data: { isDeleted: transaction } })
-    } catch (error: unknown) {
-      console.error(`Algo de errado não está certo: ${error}`)
-      return rep.status(500).send({ details: "Internal Server Error" })
-    }
+    const transaction = await this.transactionsService.delete(Number(req.params.id))
+    return rep.status(200).send({ isDeleted: transaction })
   }
 }
